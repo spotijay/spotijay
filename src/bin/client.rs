1f436faefd36a5e76207be25e6b71fe19f42ad86
Håkon H. Lerring <hakon@lerring.no>
2020-03-15 15:54:27 +0100

    basic server and client

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,3 +1,33 @@
+use tungstenite::{connect, Message};
+use url::Url;
+
+pub struct ClientApp {
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    let _app = ClientApp {
+
+    };
+
+    let (mut socket, response) =
+        connect(Url::parse("ws://localhost:3012/").unwrap()).expect("Can't connect");
+
+    println!("Connected to the server");
+    println!("Response HTTP code: {}", response.status());
+    println!("Response contains the following headers:");
+    for (ref header, _value) in response.headers() {
+        println!("* {}", header);
+    }
+
+    socket
+        .write_message(Message::Text("Hello WebSocket".into()))
+        .unwrap();
+    loop {
+        let msg = socket.read_message().expect("Error reading message");
+        println!("Received: {}", msg);
+    }
+    // socket.close(None);
 }
